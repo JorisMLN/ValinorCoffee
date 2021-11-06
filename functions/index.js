34fe129b98ebe4beb2f976802baf45a651b3cd7b
JorisMLN <monleon.joris@gmail.com>
2021-11-06 18:27:07 +0100

    Add link avec database

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,34 @@
+// VALINOR COFFEE
+const { initializeApp, applicationDefault} = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 const functions = require("firebase-functions");
 const express = require("express");
 
 const app = express();
 
 app.get("/chronicles", (req, res) => {
-  res.send(`${Date.now()}`);
+  res.send(getChronicles(db));
 });
 
 app.get("/chronicles-cached", (req, res) => {
   res.set("Cache-Control", "public, max-age=300, s-maxage=600");
-  res.send(`${Date.now()}`);
+  res.send(getChronicles(db));
 });
 
 exports.app = functions.https.onRequest(app);
+
+
+// Call Database Telperion 
+initializeApp({
+  credential: applicationDefault()
+});
+
+const db = getFirestore();
+
+async function getChronicles(db) {
+  const snapshot = await db.collection('eldarChronicles').get();
+  const eldarList = snapshot.forEach((doc) => {
+    console.log(doc.id, '=>', doc.data());
+  });
+  return eldarList;
+}
