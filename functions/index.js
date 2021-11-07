3bc96dc1db59a410733e6d4aacd8a36aec3e2501
JorisMLN <monleon.joris@gmail.com>
2021-11-07 23:33:30 +0100

    MàJ .then

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-// VALINOR COFFEE
+// V A L I N O R  C O F F E E
 const { initializeApp, applicationDefault} = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 const functions = require("firebase-functions");
@@ -7,7 +7,12 @@ const express = require("express");
 const app = express();
 
 app.get("/chronicles", (req, res) => {
-  res.send(getChronicles(db));
+
+  getChronicles(db).then(response => {
+    res.send(response);
+  });
+
+  // res.send(getChronicles(db));
 });
 
 app.get("/chronicles-cached", (req, res) => {
@@ -27,8 +32,11 @@ const db = getFirestore();
 
 async function getChronicles(db) {
   const snapshot = await db.collection('eldarChronicles').get();
-  const eldarList = snapshot.forEach((doc) => {
-    console.log(doc.id, '=>', doc.data());
-  });
+  const eldarList = [];
+  snapshot.forEach(doc => {
+    eldarList.push({id: doc.id, data: doc.data()});
+})
+
+  console.log(eldarList);
   return eldarList;
 }
