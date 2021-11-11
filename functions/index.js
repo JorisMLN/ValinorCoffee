403c80827aa2afa54f8b48d78b0bafaec081e37d
JorisMLN <monleon.joris@gmail.com>
2021-11-11 22:23:25 +0100

    Clean code

--- functions/index.js
+++ functions/index.js
@@ -1,34 +1,34 @@
-// V A L I N O R  C O F F E E
+/* ~~~~~~~~~~~~~~~~~~ | V A L I N O R  C O F F E E | ~~~~~~~~~~~~~~~~~~ */
+
 const { initializeApp, applicationDefault} = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 const functions = require("firebase-functions");
 const express = require("express");
-
 const app = express();
+initializeApp({
+  credential: applicationDefault()
+});
+const db = getFirestore();
+
+
+
+/* | --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- | */
+/* Router */
 
 app.get("/chronicles", (req, res) => {
-
+  res.set("Cache-Control", "public, max-age=300, s-maxage=600");
   getChronicles(db).then(response => {
     res.send(response);
   });
-
   // res.send(getChronicles(db));
-});
-
-app.get("/chronicles-cached", (req, res) => {
-  res.set("Cache-Control", "public, max-age=300, s-maxage=600");
-  res.send(getChronicles(db));
 });
 
 exports.app = functions.https.onRequest(app);
 
 
-// Call Database Telperion 
-initializeApp({
-  credential: applicationDefault()
-});
 
-const db = getFirestore();
+/* | --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- | */
+/* Controllers */
 
 async function getChronicles(db) {
   const snapshot = await db.collection('eldarChronicles').get();
@@ -36,7 +36,6 @@ async function getChronicles(db) {
   snapshot.forEach(doc => {
     eldarList.push({id: doc.id, data: doc.data()});
 })
-
   console.log(eldarList);
   return eldarList;
 }
