79a096ead6133096b02bf629a15a5f5ce6f5135f
JorisMLN <monleon.joris@gmail.com>
2021-11-13 01:09:40 +0100

    Fix bug version + Add route histories

--- functions/index.js
+++ functions/index.js
@@ -1,41 +1,71 @@
 /* ~~~~~~~~~~~~~~~~~~ | V A L I N O R  C O F F E E | ~~~~~~~~~~~~~~~~~~ */
 
-const { initializeApp, applicationDefault} = require('firebase-admin/app');
-const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+// const { initializeApp, applicationDefault } = require('firebase-admin/app');
+// const { getFirestore } = require('firebase-admin/firestore');
+
+const firebaseAdmin = require('firebase-admin');  
+const initializeApp = () => firebaseAdmin.initializeApp(); 
+const getFirestore = () => firebaseAdmin.firestore(); 
+
 const functions = require("firebase-functions");
 const express = require("express");
 const app = express();
+
 initializeApp({
-  credential: applicationDefault()
+  // credential: applicationDefault()
 });
 const db = getFirestore();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
 
 /* | --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- | */
 /* Router */
 
-app.get("/chronicles", (req, res) => {
+app.get("*/chronicles", (req, res) => {
   res.set("Cache-Control", "public, max-age=300, s-maxage=600");
+  // res.send(getChronicles(db));
   getChronicles(db).then(response => {
     res.send(response);
   });
-  // res.send(getChronicles(db));
 });
 
-exports.app = functions.https.onRequest(app);
+app.get("*/histories", (req, res) => {
+  res.set("Cache-Control", "public, max-age=300, s-maxage=600");
+  // res.send(getChronicles(db));
+  getHistories(db).then(response => {
+    res.send(response);
+  });
+});
 
+
+exports.app = functions.https.onRequest(app);
 
 
 /* | --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- | */
 /* Controllers */
 
-async function getChronicles(db) {
-  const snapshot = await db.collection('eldarChronicles').get();
+const getChronicles = async (db) => {
+  const snapshot = await db.collection("eldarChronicles").get();
   const eldarList = [];
   snapshot.forEach(doc => {
     eldarList.push({id: doc.id, data: doc.data()});
 })
   console.log(eldarList);
-  return eldarList;
+  return eldarList.length ? eldarList : {data: "coucou"};
+}
+
+const getHistories = async (db) => {
+  const snapshot = await db.collection("histories").get();
+  const histories = [];
+  snapshot.forEach(doc => {
+    histories.push({id: doc.id, data: doc.data()});
+})
+  console.log(histories);
+  return histories.length ? histories : {data: "coucou"};
 }
